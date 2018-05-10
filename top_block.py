1d2f894105b88a637ef24d3025674f626ac9d727
SIDDHARTH DEORE <siddharthdeore@gmail.com>
2018-05-10 22:50:04 +0200

    Add files via upload

---
+++ top_block.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Top Block
+# Generated: Thu May 10 22:40:22 2018
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from gnuradio import analog
+from gnuradio import audio
+from gnuradio import eng_notation
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio import wxgui
+from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
+from gnuradio.filter import firdes
+from gnuradio.wxgui import fftsink2
+from gnuradio.wxgui import forms
+from gnuradio.wxgui import waterfallsink2
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import osmosdr
+import time
+import wx
+
+
+class top_block(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="Top Block")
+        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
+        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.sdr_samp_rate = sdr_samp_rate = 2.4e6
+        self.samp_rate = samp_rate = 48e3
+        self.freq = freq = 88.1e6
+
+        ##################################################
+        # Blocks
+        ##################################################
+        _freq_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._freq_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_freq_sizer,
+        	value=self.freq,
+        	callback=self.set_freq,
+        	label='freq',
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._freq_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_freq_sizer,
+        	value=self.freq,
+        	callback=self.set_freq,
+        	minimum=80e6,
+        	maximum=110e6,
+        	num_steps=600,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_freq_sizer)
+        self.wxgui_waterfallsink2_0 = waterfallsink2.waterfall_sink_c(
+        	self.GetWin(),
+        	baseband_freq=0,
+        	dynamic_range=100,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=sdr_samp_rate,
+        	fft_size=512,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title='Waterfall Plot',
+        )
+        self.Add(self.wxgui_waterfallsink2_0.win)
+        self.wxgui_fftsink2_1 = fftsink2.fft_sink_c(
+        	self.GetWin(),
+        	baseband_freq=0,
+        	y_per_div=10,
+        	y_divs=10,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=sdr_samp_rate,
+        	fft_size=512,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title='FFT Plot',
+        	peak_hold=False,
+        )
+        self.Add(self.wxgui_fftsink2_1.win)
+        self.rtlsdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + '' )
+        self.rtlsdr_source_0.set_sample_rate(sdr_samp_rate)
+        self.rtlsdr_source_0.set_center_freq(freq, 0)
+        self.rtlsdr_source_0.set_freq_corr(0, 0)
+        self.rtlsdr_source_0.set_dc_offset_mode(0, 0)
+        self.rtlsdr_source_0.set_iq_balance_mode(0, 0)
+        self.rtlsdr_source_0.set_gain_mode(False, 0)
+        self.rtlsdr_source_0.set_gain(40, 0)
+        self.rtlsdr_source_0.set_if_gain(20, 0)
+        self.rtlsdr_source_0.set_bb_gain(20, 0)
+        self.rtlsdr_source_0.set_antenna('', 0)
+        self.rtlsdr_source_0.set_bandwidth(0, 0)
+          
+        self.rational_resampler_xxx_0_0 = filter.rational_resampler_ccf(
+                interpolation=1,
+                decimation=4,
+                taps=None,
+                fractional_bw=None,
+        )
+        self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
+                interpolation=48,
+                decimation=50,
+                taps=None,
+                fractional_bw=None,
+        )
+        self.low_pass_filter_0 = filter.fir_filter_ccf(1, firdes.low_pass(
+        	1, sdr_samp_rate, 100e3, 1e6, firdes.WIN_HAMMING, 6.76))
+        self.audio_sink_0 = audio.sink(48000, '', True)
+        self.analog_wfm_rcv_0 = analog.wfm_rcv(
+        	quad_rate=500e3,
+        	audio_decimation=10,
+        )
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_wfm_rcv_0, 0), (self.rational_resampler_xxx_0, 0))    
+        self.connect((self.low_pass_filter_0, 0), (self.analog_wfm_rcv_0, 0))    
+        self.connect((self.rational_resampler_xxx_0, 0), (self.audio_sink_0, 0))    
+        self.connect((self.rational_resampler_xxx_0_0, 0), (self.low_pass_filter_0, 0))    
+        self.connect((self.rtlsdr_source_0, 0), (self.rational_resampler_xxx_0_0, 0))    
+        self.connect((self.rtlsdr_source_0, 0), (self.wxgui_fftsink2_1, 0))    
+        self.connect((self.rtlsdr_source_0, 0), (self.wxgui_waterfallsink2_0, 0))    
+
+    def get_sdr_samp_rate(self):
+        return self.sdr_samp_rate
+
+    def set_sdr_samp_rate(self, sdr_samp_rate):
+        self.sdr_samp_rate = sdr_samp_rate
+        self.wxgui_waterfallsink2_0.set_sample_rate(self.sdr_samp_rate)
+        self.wxgui_fftsink2_1.set_sample_rate(self.sdr_samp_rate)
+        self.rtlsdr_source_0.set_sample_rate(self.sdr_samp_rate)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.sdr_samp_rate, 100e3, 1e6, firdes.WIN_HAMMING, 6.76))
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+        self._freq_slider.set_value(self.freq)
+        self._freq_text_box.set_value(self.freq)
+        self.rtlsdr_source_0.set_center_freq(self.freq, 0)
+
+
+def main(top_block_cls=top_block, options=None):
+
+    tb = top_block_cls()
+    tb.Start(True)
+    tb.Wait()
+
+
+if __name__ == '__main__':
+    main()
